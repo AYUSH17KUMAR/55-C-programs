7b96e7b367c88a0394c25d47871761f14151aaf5
AYUSH KUMAR <116485361+AYUSH17KUMAR@users.noreply.github.com>
2023-01-13 01:01:51 +0530

    Add files via upload

---
+++ C program to copy one array to another array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main()
+{
+   int i,m,j;
+
+   printf("Enter the size of array:");
+   scanf("%d",&m);
+   int a[m];
+   int  b[m];
+    printf("Enter the elements \n:");
+    
+    
+  for ( i = 0; i <m; i++)
+   { printf("\na[%d]=",i);
+      scanf("%d",&a[i]);
+       b[i]=a[i];}
+       printf("\nArray a[] tranfer to array b[]\n"); 
+    for(i=0;i<m;i++)
+     
+     printf("\na[%d] = %d\n",i,b[i]);
+    
+    return 0;
+}
---
+++ C program to find the sum of main diagonal elements of a matrix.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,m,n,sum=0;
+    
+    printf("Enter the rows :");
+    scanf("%d",&m);
+    
+    printf("Enter the columns :");
+    scanf("%d",&n);
+    int a[m][n];
+    if(m==n)
+    {
+        printf("Enter the elemnts in array :");
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                scanf("%d",&a[i][j]);
+            }
+        }
+
+        printf("\nCreated array :\n");
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                printf("%d ",a[i][j]);
+            }
+            printf("\n");
+        }
+
+         for(i=0;i<m;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(i==j || j+i==m-1)
+                {
+                    sum+=a[i][j];
+                }
+            }
+           
+        }
+        printf("sum of diagnal array : %d",sum);
+
+}
+return 0;
+}
